25111c91019cabbf0dd1ef6177e1252f2dd5fcd4
zilazach <105255994+zilazach@users.noreply.github.com>
2022-05-13 11:10:35 +1200

    done

--- Program.cs
+++ Program.cs
@@ -4,14 +4,20 @@ namespace Team_3
 {
     internal class Program
     {
-        public static void Declan()
+        public static void zach()
+        {
+            Console.WriteLine("Hello World. My name is zach");
+        }
+            public static void Declan()
         {
             Console.WriteLine("Hello World. My name is Declan de Haas");
-
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, my name is Alex");
+            zach();
+            Declan();
+            Console.ReadLine();
         }
     }
 }
